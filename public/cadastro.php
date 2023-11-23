73833a44af390700794df165c8c5d9390a6aae4c
Gil Guimarães <gilguimaraes2004@gmail.com>
2023-11-23 17:15:33 -0300

    .

--- public/cadastro.php
+++ public/cadastro.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (isset($_SESSION['2fa']) && $_SESSION['2fa'] == true) {
+    header("Location: public/main.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
